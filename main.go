d5b618054671195fe17b98912e431ae5e3142680
Zchary-Ma <Zcharyma@gmail.com>
2021-06-13 08:33:16 +0800

    init repo with simple test case

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Github Action")
+}
+
+func Hello(name string) string {
+	return "Hello " + name
+}
---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"test case", args{"Github"}, "Hello Github"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.args.name); got != tt.want {
+				t.Errorf("Hello() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
